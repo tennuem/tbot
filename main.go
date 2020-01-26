419b5816e09273dbf3847126d99fba611dd4e4e9
tennuem <tennuem@gmail.com>
2020-01-26 16:41:15 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+	bot.Debug = true
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+	if err != nil {
+		panic(err)
+	}
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /ping."
+			case "ping":
+				msg.Text = "pong"
+			default:
+				msg.Text = "I don't know that command"
+			}
+			bot.Send(msg)
+		}
+	}
+}
